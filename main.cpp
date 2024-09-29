9e971e176b0775a75a7615a71d20c114b6f019d8
seb hall <sebhalldesigns@gmail.com>
2024-09-29 12:03:51 +0100

    working Win32 OpenGL 3.3 example

---
+++ main.cpp
@@ -0,0 +1,323 @@
+#ifndef UNICODE
+#define UNICODE
+#endif 
+
+#include <windows.h>
+//#include <glad/glad.h>
+//#include <glad_wgl/glad_wgl.h>
+#include <GL/gl.h>
+#include <iostream>
+
+static void
+fatal_error(char *msg)
+{
+    MessageBoxA(NULL, msg, "Error", MB_OK | MB_ICONEXCLAMATION);
+    exit(EXIT_FAILURE);
+}
+
+typedef HGLRC WINAPI wglCreateContextAttribsARB_type(HDC hdc, HGLRC hShareContext,
+        const int *attribList);
+wglCreateContextAttribsARB_type *wglCreateContextAttribsARB;
+
+// See https://www.khronos.org/registry/OpenGL/extensions/ARB/WGL_ARB_create_context.txt for all values
+#define WGL_CONTEXT_MAJOR_VERSION_ARB             0x2091
+#define WGL_CONTEXT_MINOR_VERSION_ARB             0x2092
+#define WGL_CONTEXT_PROFILE_MASK_ARB              0x9126
+
+#define WGL_CONTEXT_CORE_PROFILE_BIT_ARB          0x00000001
+
+typedef BOOL WINAPI wglChoosePixelFormatARB_type(HDC hdc, const int *piAttribIList,
+        const FLOAT *pfAttribFList, UINT nMaxFormats, int *piFormats, UINT *nNumFormats);
+wglChoosePixelFormatARB_type *wglChoosePixelFormatARB;
+
+// See https://www.khronos.org/registry/OpenGL/extensions/ARB/WGL_ARB_pixel_format.txt for all values
+#define WGL_DRAW_TO_WINDOW_ARB                    0x2001
+#define WGL_ACCELERATION_ARB                      0x2003
+#define WGL_SUPPORT_OPENGL_ARB                    0x2010
+#define WGL_DOUBLE_BUFFER_ARB                     0x2011
+#define WGL_PIXEL_TYPE_ARB                        0x2013
+#define WGL_COLOR_BITS_ARB                        0x2014
+#define WGL_DEPTH_BITS_ARB                        0x2022
+#define WGL_STENCIL_BITS_ARB                      0x2023
+
+#define WGL_FULL_ACCELERATION_ARB                 0x2027
+#define WGL_TYPE_RGBA_ARB                         0x202B
+
+std::wstring utf8_to_wstring(const std::string& str) {
+    int size_needed = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), (int)str.size(), NULL, 0);
+    std::wstring wstrTo(size_needed, 0);
+    MultiByteToWideChar(CP_UTF8, 0, str.c_str(), (int)str.size(), &wstrTo[0], size_needed);
+    return wstrTo;
+}
+
+const char* VERTEX_SHADER_SOURCE = R"(
+    #version 330 core
+    layout (location = 0) in vec2 aPos;      // Vertex position (x, y) in pixels
+    layout (location = 1) in vec2 aTexCoord; // Texture coordinates (u, v)
+    layout (location = 2) in vec4 aColor;    // Background color (r, g, b, a)
+    layout (location = 3) in int textureAttachment; // texture ID (or invalid one if no texture used)
+
+    uniform vec2 viewportSize;
+
+    out vec2 TexCoord;
+    out vec4 BackgroundColor;
+    flat out int TextureIndex;
+
+    void main() {
+        vec2 unit_position = aPos / viewportSize;
+        vec2 normalized_position = (unit_position * 2) - 1;
+        normalized_position.y *= -1;
+
+        // Pass through the position
+        gl_Position = vec4(normalized_position, 0.0, 1.0);
+
+        // Pass data to fragment shader
+        TexCoord = aTexCoord;
+        BackgroundColor = aColor;
+        TextureIndex = textureAttachment;
+    }
+)";
+
+const char* FRAGMENT_SHADER_SOURCE = R"(
+    #version 330 core
+    in vec2 TexCoord;
+    in vec4 BackgroundColor;
+    flat in int TextureIndex;
+
+    uniform int activeTextures;     // How many textures are active (for safety)
+    uniform sampler2D textures[16];
+
+    out vec4 FragColor;
+
+    void main() {
+        if (TextureIndex < activeTextures) {
+            // Sample from the appropriate texture
+            FragColor = texture(textures[TextureIndex], TexCoord);
+        } else {
+            // Use background color if no texture is active
+            FragColor = BackgroundColor;
+        }
+    }
+)";
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_SIZE:
+            // Handle window resize
+            glViewport(0, 0, LOWORD(lParam), HIWORD(lParam));
+            PostMessage(hwnd, WM_PAINT, 0, 0);
+            return 0;
+        case WM_PAINT:
+
+            printf("PAINT\n");
+            // Render the scene
+            glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            // Add your rendering code here
+            SwapBuffers(GetDC(hwnd));
+            ValidateRect(hwnd, NULL);
+            return 0;
+        case WM_CLOSE:
+            PostQuitMessage(0);
+            return 0;
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+
+   return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+
+static void
+init_opengl_extensions(void)
+{
+    // Before we can load extensions, we need a dummy OpenGL context, created using a dummy window.
+    // We use a dummy window because you can only set the pixel format for a window once. For the
+    // real window, we want to use wglChoosePixelFormatARB (so we can potentially specify options
+    // that aren't available in PIXELFORMATDESCRIPTOR), but we can't load and use that before we
+    // have a context.
+    WNDCLASSA window_class = { };
+    window_class.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+    window_class.lpfnWndProc = DefWindowProcA;
+    window_class.hInstance = GetModuleHandle(0);
+    window_class.lpszClassName = "Dummy_WGL_djuasiodwa";
+
+
+    if (!RegisterClassA(&window_class)) {
+        fatal_error("Failed to register dummy OpenGL window.");
+    }
+
+    HWND dummy_window = CreateWindowExA(
+        0,
+        window_class.lpszClassName,
+        "Dummy OpenGL Window",
+        0,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        0,
+        0,
+        window_class.hInstance,
+        0);
+
+    if (!dummy_window) {
+        fatal_error("Failed to create dummy OpenGL window.");
+    }
+
+    HDC dummy_dc = GetDC(dummy_window);
+
+    PIXELFORMATDESCRIPTOR pfd = {};
+        pfd.nSize = sizeof(pfd);
+        pfd.nVersion = 1;
+        pfd.iPixelType = PFD_TYPE_RGBA;
+        pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+        pfd.cColorBits = 32;
+        pfd.cAlphaBits = 8;
+        pfd.iLayerType = PFD_MAIN_PLANE;
+        pfd.cDepthBits = 24;
+        pfd.cStencilBits = 8;
+
+    int pixel_format = ChoosePixelFormat(dummy_dc, &pfd);
+    if (!pixel_format) {
+        fatal_error("Failed to find a suitable pixel format.");
+    }
+    if (!SetPixelFormat(dummy_dc, pixel_format, &pfd)) {
+        fatal_error("Failed to set the pixel format.");
+    }
+
+    HGLRC dummy_context = wglCreateContext(dummy_dc);
+    if (!dummy_context) {
+        fatal_error("Failed to create a dummy OpenGL rendering context.");
+    }
+
+    if (!wglMakeCurrent(dummy_dc, dummy_context)) {
+        fatal_error("Failed to activate dummy OpenGL rendering context.");
+    }
+
+    wglCreateContextAttribsARB = (wglCreateContextAttribsARB_type*)wglGetProcAddress(
+        "wglCreateContextAttribsARB");
+    wglChoosePixelFormatARB = (wglChoosePixelFormatARB_type*)wglGetProcAddress(
+        "wglChoosePixelFormatARB");
+
+    wglMakeCurrent(dummy_dc, 0);
+    wglDeleteContext(dummy_context);
+    ReleaseDC(dummy_window, dummy_dc);
+    DestroyWindow(dummy_window);
+
+    printf("GOT TO HERE\n");
+}
+
+static HGLRC
+init_opengl(HDC real_dc)
+{
+    init_opengl_extensions();
+
+    // Now we can choose a pixel format the modern way, using wglChoosePixelFormatARB.
+    int pixel_format_attribs[] = {
+        WGL_DRAW_TO_WINDOW_ARB,     GL_TRUE,
+        WGL_SUPPORT_OPENGL_ARB,     GL_TRUE,
+        WGL_DOUBLE_BUFFER_ARB,      GL_TRUE,
+        WGL_ACCELERATION_ARB,       WGL_FULL_ACCELERATION_ARB,
+        WGL_PIXEL_TYPE_ARB,         WGL_TYPE_RGBA_ARB,
+        WGL_COLOR_BITS_ARB,         32,
+        WGL_DEPTH_BITS_ARB,         24,
+        WGL_STENCIL_BITS_ARB,       8,
+        0
+    };
+
+    int pixel_format;
+    UINT num_formats;
+    wglChoosePixelFormatARB(real_dc, pixel_format_attribs, 0, 1, &pixel_format, &num_formats);
+    if (!num_formats) {
+        fatal_error("Failed to set the OpenGL 3.3 pixel format.");
+    }
+
+    PIXELFORMATDESCRIPTOR pfd;
+    DescribePixelFormat(real_dc, pixel_format, sizeof(pfd), &pfd);
+    if (!SetPixelFormat(real_dc, pixel_format, &pfd)) {
+        fatal_error("Failed to set the OpenGL 3.3 pixel format.");
+    }
+
+    // Specify that we want to create an OpenGL 3.3 core profile context
+    int gl33_attribs[] = {
+        WGL_CONTEXT_MAJOR_VERSION_ARB, 3,
+        WGL_CONTEXT_MINOR_VERSION_ARB, 3,
+        WGL_CONTEXT_PROFILE_MASK_ARB,  WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
+        0,
+    };
+
+    HGLRC gl33_context = wglCreateContextAttribsARB(real_dc, 0, gl33_attribs);
+    if (!gl33_context) {
+        fatal_error("Failed to create OpenGL 3.3 context.");
+    }
+
+    if (!wglMakeCurrent(real_dc, gl33_context)) {
+        fatal_error("Failed to activate OpenGL 3.3 rendering context.");
+    }
+
+    return gl33_context;
+}
+
+
+
+int main() {
+
+    std::string CLASS_NAME = "OpenGLWindowClass";
+    std::wstring wClassName = utf8_to_wstring(CLASS_NAME);
+
+    WNDCLASS wc = {};
+    wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = GetModuleHandle(nullptr);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH) (COLOR_WINDOW + 1);
+    wc.lpszClassName = L"OpenGL Class";
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(
+        0,
+        L"OpenGL Class",
+        L"OpenGL Window",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+        NULL,
+        NULL,
+        wc.hInstance,
+        NULL
+    );
+
+    if (hwnd == NULL) {
+        return 0;
+    }
+
+    HDC gldc = GetDC(hwnd);
+    HGLRC glrc = init_opengl(gldc);
+
+    //SetupOpenGL(hwnd);
+
+    ShowWindow(hwnd, SW_SHOW);
+    UpdateWindow(hwnd);
+
+
+    bool should_quit = false;
+    MSG msg = { };
+    while (!should_quit) {
+        // Generally you'll want to empty out the message queue before each rendering
+        // frame or messages will build up in the queue possibly causing input
+        // delay. Multiple messages and input events occur before each frame.
+        while (PeekMessage(&msg, hwnd, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+
+            if (msg.message == WM_QUIT || (msg.message == WM_KEYDOWN && msg.wParam == VK_ESCAPE))
+                should_quit = true;
+        }
+
+    }
+
+
+    return 0;
+}
